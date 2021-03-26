87fe703e8d4f25cfb6d0f347a50632494c83c99d
cwd-k2 <kaito.augmented7th@gmail.com>
2021-03-26 19:51:29 +0900

    "refs/heads/master" => const main = "refs/heads/master"

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const { exec } = require("child_process");
 const app = express();
 
 const port = 3000;
+const main = "refs/heads/master";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/webhook", (req, res) => {
-  if (req.body.ref === "refs/heads/master") {
-    exec("./bin/run", (err, stdout, stderr) => {
-      if (err) {
-        console.log(stderr);
-      } else {
-        console.log(stdout);
-      }
+  if (req.body.ref === main) {
+    exec("./bin/run", (_, stdout, stderr) => {
+      console.log(stderr);
+      console.log(stdout);
     });
   }
   res.end();
